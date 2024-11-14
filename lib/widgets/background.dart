7ddbc673ae1b9c49c99cbab6cc253051683243ca
BSIT4ACapstone <capstonepioneers@gmail.com>
2024-11-14 09:47:35 +0800

    updated background.dart

--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-class CustomBackground extends StatelessWidget {
+
+class CustomBackground extends StatefulWidget {
   final Widget child;
   final String backgroundImage;
 
@@ -11,15 +15,49 @@ class CustomBackground extends StatelessWidget {
   });
 
   @override
+  State<CustomBackground> createState() => _CustomBackgroundState();
+}
+
+class _CustomBackgroundState extends State<CustomBackground> {
+  ImageProvider? _backgroundImageProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBackgroundImage();
+  }
+
+  Future<void> _fetchBackgroundImage() async {
+    try {
+      final storageRef =
+      FirebaseStorage.instance.ref().child(widget.backgroundImage);
+      final downloadUrl = await storageRef.getDownloadURL();
+      log('Download URL: $downloadUrl'); // Log the download URL for debugging
+
+      setState(() {
+        _backgroundImageProvider = NetworkImage(downloadUrl);
+      });
+    } catch (e) {
+      log('Error fetching image: $e');
+      // In case of error, use the default image
+      setState(() {
+        _backgroundImageProvider = const AssetImage('assets/newbg.jpg');
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage(backgroundImage),
+          image: _backgroundImageProvider ??
+              const AssetImage(
+                  'assets/newbg.jpg'), // Default placeholder image
           fit: BoxFit.cover,
         ),
       ),
-      child: child,
+      child: widget.child,
     );
   }
 }
